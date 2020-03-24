7f4cf87560d08db77d8ab3c49c8daae1a26d8e81
Pure Engineering <sashi@pureengineering.com>
2020-03-23 22:27:42 -0700

    fixed console for negative numbers, added temperature, some cleanup

--- firmware/linux_demo/main.c
+++ firmware/linux_demo/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
 #include <sys/types.h>
@@ -9,7 +10,7 @@
 #include <linux/i2c-dev.h>
 #include <linux/i2c.h>
 
-typedef unsigned char u8;
+// typedef unsigned char uint8_t;
 
 // Global file descriptor used to talk to the I2C bus:
 int i2c_fd = -1;
@@ -24,7 +25,7 @@ int i2c_init(void)
     if ((i2c_fd = open(i2c_fname, O_RDWR)) < 0)
     {
         char err[200];
-        sprintf(err, "open('%s') in i2c_init", i2c_fname);
+        sprintf(err, "open('%s') in i2c_init, (modify i2c_fname)", i2c_fname);
         perror(err);
         return -1;
     }
@@ -33,10 +34,10 @@ int i2c_init(void)
 }
 
 // Write to an I2C slave device's register:
-int i2c_write(u8 slave_addr, u8 reg, u8 data)
+int i2c_write(uint8_t slave_addr, uint8_t reg, uint8_t data)
 {
     int retval;
-    u8 outbuf[2];
+    uint8_t outbuf[2];
 
     struct i2c_msg msgs[1];
     struct i2c_rdwr_ioctl_data msgset[1];
@@ -62,10 +63,10 @@ int i2c_write(u8 slave_addr, u8 reg, u8 data)
 }
 
 // Read the given I2C slave device's register and return the read value in `*result`:
-int i2c_read(u8 slave_addr, u8 reg, u8 *result)
+int i2c_read(uint8_t slave_addr, uint8_t reg, uint8_t *result)
 {
     int retval;
-    u8 outbuf[1], inbuf[1];
+    uint8_t outbuf[1], inbuf[1];
     struct i2c_msg msgs[2];
     struct i2c_rdwr_ioctl_data msgset[1];
 
@@ -97,14 +98,17 @@ int i2c_read(u8 slave_addr, u8 reg, u8 *result)
     return 0;
 }
 
+//to compile and run: gcc main.c ; sudo ./a.out
+//modify i2c_fname to match i2c port
 void main()
 {
     int i, j;
-    u8 lower_byte;
-    u8 upper_byte;
-    u8 buffer[64];
+    uint8_t lower_byte;
+    uint8_t upper_byte;
+    uint8_t buffer[64];
     int pixel;
-    unsigned short image[64];
+    signed short image[64];
+    float temperature[64];
 
     i2c_init();
 
@@ -117,25 +121,33 @@ void main()
             i2c_read(I2C_SLAVE_ADDR, 0x80 + i * 2 + 1, &upper_byte);
 
             image[i] = ((upper_byte << 8) | lower_byte);
+            if (image[i] > 2047)
+            {
+                image[i] = image[i] - 4096;
+            }
+            temperature[i] = image[i] * .25;
         }
 
-        printf("\033[H\033[J");
+        printf("\033[H\033[J"); //clear screen
+        printf("Raw Values\n");
         for (i = 0; i < 8; i++)
         {
             for (j = 7; j >= 0; j--)
             {
                 pixel = image[i * 8 + j];
-                if (pixel > 99)
-                {
-                    printf("%03d ", image[i * 8 + j]);
-                }
-                else
-                {
-                    printf(" %02d ", image[i * 8 + j]);
-                }
+                printf("%*d ",3, pixel);
+            }
+            printf("\n");
+        }
+
+        printf("\nTemperature\n");
+        for (i = 0; i < 8; i++)
+        {
+            for (j = 7; j >= 0; j--)
+            {
+                printf("%6.2f ", temperature[i * 8 + j]);
             }
             printf("\n");
         }
     }
-    // close(i2c_fd);
 }
